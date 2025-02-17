499175299996f89b2338a0c727048e26bfb7dda2
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-02-17 22:15:26 +0530

    first numerical code in python

---
+++ first_code.py
@@ -0,0 +1,11 @@
+def math_op():
+    div=5/2
+    div_1=7//2
+    module=9%5
+    power= 7**2
+    return[div, div_1, module, power]
+[div, div_1, module, power]= math_op()
+print(div)
+print(div_1)
+print(module)
+print(power)
