b54e6a1315c1171a7e887450f4ec3a4a5dc96344
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-02-21 21:50:06 +0530

    score grade

---
+++ marks_system.py
@@ -0,0 +1,15 @@
+score= int(input("Enter the score : "))
+
+if score>75100:
+    grade="not exist"
+elif score>90:
+    grade= "A"
+elif score>80:
+    grade= "B"
+elif score>70:
+    grade= "C"
+elif score>60:
+    grade= "D"
+else:
+    grade= "F"
+print ("This is your Grade :", grade)
