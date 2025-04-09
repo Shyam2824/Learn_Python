965cad68ac6c95b479ea7a5261a4ad56d6e3b5e8
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-04-09 21:08:12 +0530

    operator and logical operators

---
+++ operator01.py
@@ -0,0 +1,29 @@
+# Arithematic 
+a= 20
+b=10
+print(a,b)
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+print (a%b)
+print (a<b)
+print (a>b)
+print (a==b)
+print (a!=b)
+
+# logical operator
+
+print("these are logical operator")
+a=True
+b= False
+print ("This is AND case")
+print(a&a)
+print(b&a)
+print(a&b)
+print(b&b) 
+print ("This is OR case")  
+print(a|a)
+print(a|b)
+print(b|a)
+print(b|b)
