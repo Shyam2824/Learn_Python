259c503166cbdc32ed07b72c941c18591118ff40
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-04-13 08:25:34 +0530

    elif lader

--- conditional_01.py
+++ conditional_01.py
@@ -1,7 +1,13 @@
 age= int(input("Enter your age : "))
 
-if age>= 18:
+if age== 18 & age<=59:
     print("Yes , you are the part of member")
+    
+elif age<=0:
+    print("it is cant valid age ,Please check it ")
+    
+elif age>60:
+    print("your are older member of my team")
 
 else:
     print("no, you are not part of member")
