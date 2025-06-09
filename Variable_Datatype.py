b2c434ab7675c3c48847983d952777006ccfec15
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-06-09 13:34:14 +0530

    data type and variable

---
+++ Variable_Datatype.py
@@ -0,0 +1,21 @@
+#   DataType and creating a variable
+
+x=5
+x=12
+print (x) #there is two variable in same name that call latest one
+
+
+# Different type of data type
+
+a=10
+b=2.5
+c="sohan,shyam"
+print(a, b, c)
+
+
+y="we are playing with python"
+print(y)
+
+# Direct print without any variable
+
+print("we all are learning python")
