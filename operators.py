b37eef84b587da08c35b28cbd906476b2adbbeb0
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-06-14 12:14:48 +0530

    practice

---
+++ operators.py
@@ -0,0 +1,19 @@
+#types of operators
+# Arithimetic operators: + - * /,% //, **
+# boolean operators : true or false
+# compression operators : <,>,==,>=
+#logical operators : and, or, not
+# assignment operators : ==, +=, -= etc
+
+#  practice question
+
+#  question 1: sum of all product 
+
+p1=5*250
+p2=25*625
+p3= 34*274
+
+print("the product 1", p1)
+print("the product 2", p2)
+print("the product 3", p3)
+print("Sum of all product ", p1+p2+p3)
