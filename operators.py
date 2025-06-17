e12655250d1e96de6e4af3572c12178a645a9e53
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-06-17 22:22:54 +0530

    practice

--- operators.py
+++ operators.py
@@ -17,3 +17,17 @@ print("the product 1", p1)
 print("the product 2", p2)
 print("the product 3", p3)
 print("Sum of all product ", p1+p2+p3)
+
+# question 2  Simple interest
+# si= (p*r*t)/100
+
+r=8
+t=3
+p=15000
+si= ((p*r*t)/100)
+print(si)
+
+# question 3 area of circle
+
+r=12
+print("area of circle", (22/7)*r*r)
