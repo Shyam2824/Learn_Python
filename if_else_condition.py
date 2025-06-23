9984e2399407f439c8013f9a8b684f3e85b07db2
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-06-23 22:01:25 +0530

    if else condition problem

--- if_else_condition.py
+++ if_else_condition.py
@@ -1,13 +1,24 @@
 #calculate the temperature input taken from user
-# temp= float(input("Enter the temperature "))
-# if temp <= 15:
-#     print("temperature is not convinient ")
-# if 24 <= temp <= 28:
-#     print("temperature is convinient ")
+temp= float(input("Enter the temperature "))
+if temp <= 15:
+    print("temperature is not convinient ")
+if 24 <= temp <= 28:
+    print("temperature is convinient ")
     
-# age to election or elegible to vote
+# # age to election or elegible to vote
 age=float(input("Enter your age"))
 if age>=18:
     print("you are elegible to vote")
 else:
     print("you are not elegible to vote")
+
+# net salary
+base_salary= float(input("Enter your base salary"))
+year_service= int(input("Enter your year of service"))
+
+if year_service>5:
+    total_salary= base_salary + (0.08*base_salary) - (0.12*base_salary)
+    print("You will get net salary " , total_salary)
+else:
+    net_salary= base_salary-(0.12*base_salary)
+    print("you will get net salary ", net_salary)
