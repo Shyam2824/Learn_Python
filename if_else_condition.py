61e2ca79c77034dae2c3b9f35ebce75ff9dde070
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-06-23 21:45:40 +0530

    temperature condition

---
+++ if_else_condition.py
@@ -0,0 +1,6 @@
+#calculate the temperature input taken from user
+temp= float(input("Enter the temperature "))
+if temp <= 15:
+    print("temperature is not convinient ")
+if 24 <= temp <= 28:
+    print("temperature is convinient ")
