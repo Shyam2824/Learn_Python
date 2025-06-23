74e47a11043f2aaa42e0115edb6d19c49740b613
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-06-23 21:50:07 +0530

    elegible to vote

--- if_else_condition.py
+++ if_else_condition.py
@@ -1,6 +1,13 @@
 #calculate the temperature input taken from user
-temp= float(input("Enter the temperature "))
-if temp <= 15:
-    print("temperature is not convinient ")
-if 24 <= temp <= 28:
-    print("temperature is convinient ")
+# temp= float(input("Enter the temperature "))
+# if temp <= 15:
+#     print("temperature is not convinient ")
+# if 24 <= temp <= 28:
+#     print("temperature is convinient ")
+    
+# age to election or elegible to vote
+age=float(input("Enter your age"))
+if age>=18:
+    print("you are elegible to vote")
+else:
+    print("you are not elegible to vote")
