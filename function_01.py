cb77741e9848eab575ecadc3a746e08ccbc4ebfc
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-07-01 22:41:36 +0530

    function

---
+++ function_01.py
@@ -0,0 +1,11 @@
+def my_function():
+    print("the last date of bill payment is 1st week ")
+    print("the last date of bill payment is final week ")
+    print("the last date of bill payment is  week ")
+    
+for i in range(3):
+    a=input(" enter your name :")
+    b= int(input(" enter the units  "))
+    bill= b*10
+    print("total bill ", bill)
+    my_function()
